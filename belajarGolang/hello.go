0fcf10b7227cabf65ef91932fa36f33d946d13df
ricky lai <ricky.abclab@gmail.com>
2020-01-24 16:46:53 +0700

    test
    test

---
+++ belajarGolang/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	z "fmt"
+)
+
+func main() {
+	z.Println("hello world")
+	z.Println(helper)
+}
---
+++ belajarGolang/helper/hitung.go
@@ -0,0 +1,9 @@
+package helper
+
+func Tambah(x int, y int) int {
+	return x + y
+}
+
+func Kurang(x int, y int) int {
+	return x - y
+}
